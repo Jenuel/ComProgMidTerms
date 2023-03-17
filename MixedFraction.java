b992282182a9bcb1079ff6c5c41c7fe665daa529
sherynac <92623214+sherynac@users.noreply.github.com>
2023-03-17 10:57:04 +0800

    Update MixedFraction.java

--- MixedFraction.java
+++ MixedFraction.java
@@ -1,76 +1,87 @@
 package prog2.prelimgroup;
 
-public class MixedFraction extends Fraction{
+public class MixedFraction extends Fraction {
 
     private int wholeNumber;
 
-    public MixedFraction(){
+    public MixedFraction() {
         super();
-        this.wholeNumber=0;
+        this.wholeNumber = 0;
     }
 
-    public MixedFraction( int num , int den, int wholeNumber){
+    public MixedFraction(int num, int den, int wholeNumber) {
         super(num, den);
-        this.wholeNumber=wholeNumber;
+        this.wholeNumber = wholeNumber;
     }
-
-    public MixedFraction (Fraction fraction) {
+    public MixedFraction (int wholeNumber, Fraction fraction){
         super();
-    }
-
-    public void setWholeNumber(int wholeNumber){
         this.wholeNumber = wholeNumber;
     }
 
-    public void setFraction(Fraction fraction){
+    public MixedFraction(Fraction fraction) {
+        super();
+    }
+
+    public void setWholeNumber(int wholeNumber) {
+        this.wholeNumber = wholeNumber;
+    }
+
+    public void setFraction(Fraction fraction) {
 
     }
 
-    public int getWholeNumber(){return wholeNumber;}
+    public int getWholeNumber() {
+        return wholeNumber;
+    }
 
-    public Fraction getFraction(){ return new Fraction();
+    public Fraction getFraction() {
+        return new Fraction();
 
     }
 
-    public String toString(){
-        return this.wholeNumber+ " " + super.toString();
-
+    public String toString() {
+        if (wholeNumber == 0)
+            return " " + super.toString();
+        else
+            return this.wholeNumber + " " + super.toString();
     }
+
+    public Fraction mixedToImproper() {
+        return new Fraction(this.getWholeNumber() * this.getDenominator() + this.getNumerator(), this.getDenominator());
+    }
+
 
     public MixedFraction add (MixedFraction other) {
-        int sWholeNumber;
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction();
+
+        frac1 = this.mixedToImproper();
+        frac2 = other.mixedToImproper();
+
         Fraction sFrac = new Fraction();
-        sWholeNumber = this.getWholeNumber() + other.getWholeNumber();
-        sFrac = this.addFraction(other);
+        sFrac = frac1.addFraction(frac2);
         sFrac = sFrac.reduceFraction(sFrac);
-        return new MixedFraction(sFrac.getNumerator(),sFrac.getDenominator(), sWholeNumber);
 
+        MixedFraction sMixed = new MixedFraction();
+        sMixed = sFrac.improperToMixed();
+
+        return new MixedFraction(sMixed.getNumerator(), sMixed.getDenominator(), sMixed.getWholeNumber());
     }
 
-    public Fraction mixedToImproper(){
-        return new Fraction(this.getWholeNumber() * this.getDenominator() + this.getNumerator() ,this.getDenominator());
-    }
+    public MixedFraction subtract(MixedFraction other) {
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction();
 
-    public MixedFraction improperToMixed(){
-        return new MixedFraction(this.getNumerator() % this.getDenominator(), this.getDenominator(), this.getNumerator() / this.getDenominator());
-    }
+        frac1 = this.mixedToImproper();
+        frac2 = other.mixedToImproper();
 
-    public MixedFraction subtract ( MixedFraction other){
-        Fraction frac1= new Fraction();
-        Fraction frac2= new Fraction();
-
-        frac1= this.mixedToImproper();
-        frac2= other.mixedToImproper();
-
-        Fraction dFrac= new Fraction();
-
-        dFrac= frac1.subtractFraction(frac2);
-        dFrac= dFrac.reduceFraction(dFrac);
+        Fraction dFrac = new Fraction();
+        dFrac = frac1.subtractFraction(frac2);
+        dFrac = dFrac.reduceFraction(dFrac);
 
         MixedFraction dMixed = new MixedFraction();
-        dMixed = (MixedFraction) dFrac;
-        dMixed = dMixed.improperToMixed();
+        dMixed = dFrac.improperToMixed();
 
-        return new MixedFraction(dMixed.getNumerator(), dMixed.getDenominator(),dMixed.getWholeNumber() );
-
+        return new MixedFraction(dMixed.getNumerator(), dMixed.getDenominator(), dMixed.getWholeNumber());
     }
+}
