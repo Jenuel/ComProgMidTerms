55c7a8745accc8a6b3f24d51c4ea9b772d72bf73
sherynac <92623214+sherynac@users.noreply.github.com>
2023-03-27 10:28:33 +0800

    Update Fraction.java

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Group Members:
  * Alvarez, Maria Alexandra
  * Balogo, Renuel Jeremi
@@ -8,65 +8,67 @@
  *
  * Class Code and Schedule: 9315 CS122 MTh 9:00 - 10:30
  */
-package prog2.prelimgroup;
 
-import java.util.Scanner;
+package midterms;
+
 public class Fraction {
 
-    /*
-     * Data Members
+    /**
+     * Declare data members as private for encapsulation
      */
     private int numerator;
     private int denominator;
 
-    /*
-     * Default Constructor
+    /**
+     * Declare default Constructor
      */
     public Fraction(){
         numerator = 0;
         denominator = 1;
-    }
+    } // end of Fraction method
 
-    /*
-     * Constructor with parameters
+    /**
+     * Create a constructor with parameters
      */
     public Fraction(int num, int den){
         numerator = num;
         denominator = den;
-    }
+    } // end of Fraction method
 
+    /**
+    * Create a Fraction method where denominator is 1 and the numerator is whole number value
+    */
     public Fraction(int wholeNumVal){
         numerator = wholeNumVal;
         denominator = 1;
-    }
-    /*
-     * Set the numerator
-     */
-    public void setNumerator(int num){numerator = num;}
+    } // end of Fraction method
 
-    /*
-     * Set the denominator
+    /**
+     * Create a setter method for your numerator
+     */
+    public void setNumerator(int num){numerator = num;} // end of setNumerator method
+
+    /**
+     * Create a setter method for your denominator
      */
     public void setDenominator(int den){
         denominator = den;
-    }
+    } // end of setDenominator method
 
-    /*
-     * Get the numerator
+    /**
+     *  Create a getter method for your numerator
      */
-    public int getNumerator(){
-        return numerator;
-    }
+    public int getNumerator(){return numerator;} // end of getNumerator method
 
-    /*
-     * Get the denominator
+    /**
+     * Create a getter method for your denominator
      */
     public int getDenominator(){
         return denominator;
-    }
+    } // end of getDenominator method
 
-    /*
-     * Concatenate the variables
+    /**
+     * Create a toString method for the string representation of a fraction
      */
     public String toString(){
         if (numerator == 0 && denominator != 0) {
@@ -75,64 +77,28 @@ public class Fraction {
             return numerator + "";
         } else
             return numerator + "/" + denominator;
-    }
+    }  // end of toString method
 
-    /*
-     * Convert Fraction to Decimal
+    /**
+     * Create a method to Convert Fraction to Decimal
      */
     public double toDouble(){
         return (double) numerator/denominator;
     }
 
-
-    /*
-     * Compute for greatest common divisor (gcd)
+    /**
+     * Create a recursive method to compute for greatest common divisor (gcd)
      */
     private int computeGCD(int a, int b) {
         if (b == 0) {
             return a;
         }
         return computeGCD(b, a % b);
-    }
+    } // end of computeGCD method
 
-    /*
-     * Read/Accept a Fraction
+    /**
+     * Create a method where you can Add Fractions
      */
-    public Fraction readAFraction() {
-        Scanner keyboard = new Scanner(System.in);
-        int rNum = 0;
-        int rDen = 0;
-        boolean validNumberRead = false;
-
-        while (!validNumberRead) {
-            try {
-                System.out.print("Enter a numerator: ");
-                rNum = Integer.parseInt(keyboard.nextLine());
-                validNumberRead = true;
-            } catch (NumberFormatException exception) {
-                System.out.println("You have to enter a number");
-            }
-        }
-
-        validNumberRead = false;
-        while (!validNumberRead){
-            try {
-                System.out.print("Enter a denominator: ");
-                rDen = Integer.parseInt(keyboard.nextLine());
-                if (rDen == 0)
-                    System.out.println("Denominator must not be equal to 0");
-                else validNumberRead = true;
-            } catch (NumberFormatException exception){
-                System.out.println("You have to enter a number");
-            }
-        }
-        return new Fraction(rNum, rDen);
-    }
-
-    /*
-     * Addition of Fractions
-     */
-
     public Fraction addFraction (Fraction fraction2){
         int computedNum;
         int computedDen;
@@ -141,11 +107,10 @@ public class Fraction {
         computedDen = this.getDenominator() * fraction2.getDenominator();
 
         return new Fraction(computedNum, computedDen);
-    }
+    } // end of addFraction method
 
-
-    /*
-     * Subtraction of Fractions
+    /**
+     * Create a method to subtract Fractions
      */
     public Fraction subtractFraction(Fraction fraction2){
         int computedNum;
@@ -155,11 +120,11 @@ public class Fraction {
         computedDen = this.getDenominator() * fraction2.getDenominator();
 
         return new Fraction(computedNum, computedDen);
-    }
+    } // end of subtractFraction method
 
-    /*
-     * Multiplication of Fractions
-     */
+    /**
+     * Create a method where you can Multiply Fraction
+     **/
     public Fraction multiplyFraction(Fraction fraction2){
         int productNumerator;
         int productDenominator;
@@ -168,10 +133,10 @@ public class Fraction {
         productDenominator = this.getDenominator() * fraction2.getDenominator();
 
         return new Fraction(productNumerator, productDenominator);
-    }
+    } // end of multiplyFraction method
 
-    /*
-     * Division of Fractions
+    /**
+     * Create a method to Divide Fractions
      */
     public Fraction divideFraction(Fraction fraction2){
         int computedNum;
@@ -181,10 +146,10 @@ public class Fraction {
         computedDen = this.getDenominator() * fraction2.getNumerator();
 
         return new Fraction(computedNum, computedDen);
-    }
+    } // end of divideFraction method
 
-    /*
-     * Reduce a Fraction
+    /**
+     * Create a method to Reduce a Fraction
      */
     public Fraction reduceFraction(){
         int gcf;
@@ -197,12 +162,16 @@ public class Fraction {
         reducedDenominator = this.getDenominator() / gcf;
 
         return new Fraction(reducedNumerator, reducedDenominator);
-    }
+    } // end of reduceFraction method
 
+    /**
+     * Create a method that will convert an improper fraction to mixed fraction
+     */
     public MixedFraction improperToMixed() {
         if (numerator % denominator == 0)
             return new MixedFraction(this.numerator, this.getDenominator(), 0);
         else
-            return new MixedFraction(Math.abs(this.getNumerator() % this.getDenominator()), this.getDenominator(), this.getNumerator() / this.getDenominator());
-    }
-}
+            return new MixedFraction(Math.abs(this.getNumerator() % this.getDenominator()), this.getDenominator(),
+                    this.getNumerator() / this.getDenominator());
+    } // end of improperToMixed method
+} // end of Fraction class
