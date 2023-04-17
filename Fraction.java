f2bca50b14a3f1232db6a70be336658a8c457eba
Alexandra Alvarez <127579418+Alexamira@users.noreply.github.com>
2023-04-18 00:12:40 +0800

    Algorithm comments for Fraction.java
    Multi-line documentation comments
    Note: pa double check pu if tama pagkakaintindi ko sa improperToMixed ಥ_ಥ

--- Fraction.java
+++ Fraction.java
@@ -10,16 +10,20 @@
 
 package prog2.prelimgroup;
 
+/**
+ * Superclass for object-type Fraction
+ */
 public class Fraction {
 
     /**
-     * Declare data members as private for encapsulation
+     * Declares data members as private for encapsulation
      */
     private int numerator;
     private int denominator;
 
     /**
-     * Declare default Constructor
+     * Creates no-argument constructor
+     * <p>Initializes numerator and denominator</p>
      */
     public Fraction(){
         numerator = 0;
@@ -27,7 +31,9 @@ public class Fraction {
     } // end of Fraction method
 
     /**
-     * Create a constructor with parameters
+     * Creates a Fraction parameterized constructor
+     * @param num The numerator
+     * @param den The denominator
      */
     public Fraction(int num, int den){
         numerator = num;
@@ -35,7 +41,10 @@ public class Fraction {
     } // end of Fraction method
 
     /**
-     * Create a Fraction method where denominator is 1 and the numerator is whole number value
+     * Creates parameterized constructor for whole number args
+     * <p>Initializes numerator to the value of the whole number,
+     * and the denominator to 1</p>
+     * @param wholeNumVal The whole number value
      */
     public Fraction(int wholeNumVal){
         numerator = wholeNumVal;
@@ -43,31 +52,39 @@ public class Fraction {
     } // end of Fraction method
 
     /**
-     * Create a setter method for your numerator
+     * Sets the numerator value
+     * @param num The numerator value
      */
     public void setNumerator(int num){numerator = num;} // end of setNumerator method
 
     /**
-     * Create a setter method for your denominator
+     * Sets the denominator value
+     * @param den The denominator value
      */
     public void setDenominator(int den){
         denominator = den;
     } // end of setDenominator method
 
     /**
-     *  Create a getter method for your numerator
+     * Gets the numerator value
+     * @return <code> int </code> specifying the numerator value
      */
     public int getNumerator(){return numerator;} // end of getNumerator method
 
     /**
-     * Create a getter method for your denominator
+     * Gets the denominator value
+     * @return <code> int </code> specifying the denominator value
      */
     public int getDenominator(){
         return denominator;
     } // end of getDenominator method
 
     /**
-     * Create a toString method for the string representation of a fraction
+     * Returns a string format of the fraction.
+     * <p>If the numerator is 0, fraction string becomes "0".</p>
+     * <p>If the denominator is 1, string is the "numerator".</p>
+     * <p>Otherwise, string uses "/" to separate numerator and denominator.</p>
+     * @return <code> string </code> representing the fraction
      */
     public String toString(){
         if (numerator == 0 && denominator != 0) {
@@ -79,14 +96,23 @@ public class Fraction {
     }  // end of toString method
 
     /**
-     * Create a method to Convert Fraction to Decimal
+     * Returns a decimal form of the fraction.
+     * @return <code> double </code> representing fraction in
+     * decimal form by dividing numerator with denominator
      */
     public double toDouble(){
         return (double) numerator/denominator;
     }
 
     /**
-     * Create a recursive method to compute for greatest common divisor (gcd)
+     * Recursively computes the greatest common divisor (GCD).
+     * <p>If either number is zero, the GCD is the other number.</p>
+     * <p>Otherwise, the method recursively calls itself with the
+     * second number, and the remainder of the first number divided
+     * by the second number.</p>
+     * @param a The first number value
+     * @param b The second number value
+     * @return <code> int </code> representing the GCD of two values
      */
     private int computeGCD(int a, int b) {
         if (b == 0) {
@@ -96,7 +122,14 @@ public class Fraction {
     } // end of computeGCD method
 
     /**
-     * Create a method where you can Add Fractions
+     * <h3>Adds Fractions</h3>
+     * <p>Computes the numerator by adding the products from multiplying
+     * each fraction's numerator with the other fraction's denominator.</p>
+     * <p>Computes the denominator by multiplying each fraction's
+     * denominator.</p>
+     * @param fraction2 Fraction to add to this fraction
+     * @return <code> Fraction </code> representing the new Fraction
+     * that is the sum of two Fractions.
      */
     public Fraction addFraction (Fraction fraction2){
         int computedNum;
@@ -109,7 +142,14 @@ public class Fraction {
     } // end of addFraction method
 
     /**
-     * Create a method to subtract Fractions
+     * <h3>Subtracts Fractions</h3>
+     * <p>Computes the numerator by subtracting the products from
+     * each fraction's numerator by the other fraction's denominator.</p>
+     * <p>Computes the denominator by multiplying each fraction's
+     * denominator.</p>
+     * @param fraction2 Fraction to subtract from this fraction
+     * @return <code> Fraction </code> representing the new Fraction
+     * that is the difference of two Fractions.
      */
     public Fraction subtractFraction(Fraction fraction2){
         int computedNum;
@@ -122,8 +162,15 @@ public class Fraction {
     } // end of subtractFraction method
 
     /**
-     * Create a method where you can Multiply Fraction
-     **/
+     * <h3>Multiplies Fractions</h3>
+     * <p>Computes the numerator by multiplying each fraction's
+     * numerator.</p>
+     * <p>Computes the denominator by multiplying each fraction's
+     * denominator.</p>
+     * @param fraction2 Fraction to multiply to this fraction
+     * @return <code> Fraction </code> representing the new Fraction
+     * that is the product of two Fractions.
+     */
     public Fraction multiplyFraction(Fraction fraction2){
         int productNumerator;
         int productDenominator;
@@ -135,7 +182,14 @@ public class Fraction {
     } // end of multiplyFraction method
 
     /**
-     * Create a method to Divide Fractions
+     * <h3>Divides Fractions</h3>
+     * <p>Computes the numerator by multiplying the numerator of
+     * this fraction to the denominator of the other fraction.</p>
+     * <p>Computes the denominator by multiplying the denominator of
+     * this fraction to the numerator of the other fraction.</p>
+     * @param fraction2 Fraction to divide by this fraction
+     * @return <code> Fraction </code> representing the new Fraction
+     * that is the quotient of two Fractions.
      */
     public Fraction divideFraction(Fraction fraction2){
         int computedNum;
@@ -148,7 +202,11 @@ public class Fraction {
     } // end of divideFraction method
 
     /**
-     * Create a method to Reduce a Fraction
+     * <h3>Reduces a Fraction to its lowest term</h3>
+     * <p>Divides the numerator, and denominator by the GCD of the
+     * numerator and denominator to compute their lowest terms.</p>
+     * @return <code> Fraction </code> representing the new Fraction
+     * that is the reduced Fraction.
      */
     public Fraction reduceFraction(){
         int reducedNumerator;
@@ -160,8 +218,21 @@ public class Fraction {
         return new Fraction(reducedNumerator, reducedDenominator);
     } // end of reduceFraction method
 
-    /**
-     * Create a method that will convert an improper fraction to mixed fraction
+ /**
+     * Returns a mixed fraction form of the fraction.
+     * <p>If the numerator is a multiple of the denominator, the
+     * mixed fraction with an equivalent fraction part and
+     * no whole number is returned.</p>
+     * <p>If the denominator is of negative value, the method returns
+     * mixed fraction with:
+     * <p>- absolute value of numerator (<code>numerator%denominator</code>),
+     * <p>- absolute value of denominator,
+     * <p>- and whole number (<code>numerator / denominator</code>).</p>
+     * <p>Otherwise, computation of mixed fraction parameters from the second
+     * condition retains but the denominator doesn't have to be of absolute
+     * value.</p>
+     * @return <code> MixedFraction </code> representing the converted
+     * new Mixed Fraction
      */
     public MixedFraction improperToMixed() {
         if (numerator % denominator == 0)
